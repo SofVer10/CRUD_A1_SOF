af46da702579581fadce2b900cfe8d9fb4ab321b
Sofi <20230106@ricaldone.edu.sv>
2024-06-18 23:02:02 -0600

    Diseño de la pantalla de registro terminado

--- app/src/main/java/sofia/palacios/crud_a1_sof/pantalla_bienvenida.kt
+++ app/src/main/java/sofia/palacios/crud_a1_sof/pantalla_bienvenida.kt
@@ -1,10 +1,16 @@
 package sofia.palacios.crud_a1_sof
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class pantalla_bienvenida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,23 @@ class pantalla_bienvenida : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnContinuarBv = findViewById<Button>(R.id.btnContinuarBv)
+
+
+        btnContinuarBv.setOnClickListener {
+
+            GlobalScope.launch (Dispatchers.Main){
+                //Tiempo de cambio 3 segundos
+                delay(3000)
+
+                //Cambio de pantalla
+                val pantallaSiguiente = Intent(this@pantalla_bienvenida, activity_registro::class.java)
+                startActivity(pantallaSiguiente)
+                //Final del activity
+                finish()
+            }
         }
     }
 }
